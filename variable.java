ccc848e2b37034ef5c40796ffde5111a82f9b224
Nikita Tayade <ntayade9322@gmail.com>
2026-08-24 21:44:51 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,20 @@
+public class variable {
+    public static void main(String[] args){
+        // variable start with alphabate or _ or $
+        //special charector are not allowed except _ and $
+        //blank and comma are not allowed
+        //keyword are not allowed
+        //variable name cann't start with number
+        int num1=90;
+        int num2=900;
+        int _num=76;
+        int $num=98;
+        int Num1=76;
+        int NUM1=75;
+        int nUM=377;
+        //All the above variable are valid
+        System.out.println(num1);
+        System.out.println(NUM1);
+        
+    }
+}
