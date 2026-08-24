c51d66dba8e3ea6415db40293275c0051c143040
Nikita Tayade <ntayade9322@gmail.com>
2026-08-24 21:12:37 +0530

    Implement Arithmetic operations in Arithmetic.java
    This class demonstrates basic arithmetic operations and variable declarations in Java.

---
+++ Arithmetic.java
@@ -0,0 +1,56 @@
+public class Arithmetic {
+    public static void main(String[] args){
+        System.out.println("Nikita Tayade!!!"); // it is used to print the statement ...
+
+        System.out.println(340);//take the input as the integer
+        System.out.println("340");//take input as String
+
+        System.out.println(340+10);//take the input as the integer------> perform the arithmetic operation
+        System.out.println("340+10");//take input as String
+
+        System.out.println(340);//take the input as the integer
+        System.out.println("340");//take input as String
+
+        System.out.print(340);// print()--> same line
+        System.out.println("340");//take input as String
+
+        //variable declartion
+        //data_type variable_name=value;
+        // 1 ---> daclaration
+        int b=10;
+        System.out.print("the value of the x is :"+b);
+
+        //2 -----------> declaration
+        int a;
+        a=20;
+        System.out.print(a);
+
+        //ARITHMETIC OPERATION
+
+        double x = 45;
+        double y=9 ;
+
+        //Addition
+        System.out.println(x+y);
+        //Substraction
+        System.out.println(x-y);
+        //Multipication
+        System.out.println(x*y);
+        //division
+        System.out.println(y/x);
+        //Power
+       // System.out.println(x**y);
+        //Modulo
+        System.out.println(x%y);
+
+
+
+
+
+
+
+
+
+
+    }
+}
