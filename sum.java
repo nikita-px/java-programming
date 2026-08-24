82ca17a0d4bec0aea2885afce3597770ed78abf4
Nikita Tayade <ntayade9322@gmail.com>
2026-08-24 22:49:41 +0530

    Add sum.java to calculate the sum of three numbers

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args){
+
+        //take an input from the user
+
+        Scanner sc= new Scanner(System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+
+        double res=a+b+c;
+        System.out.println("Sum of the number is :"+res);
+
+    }
+
+}
