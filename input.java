689d759b5141ea9cef501609c4f3f4819acf6278
Nikita Tayade <ntayade9322@gmail.com>
2026-08-24 22:05:49 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+
+        //calculating the area of the circle ...
+
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+         double res=3.141*r*r;
+        System.out.println("the area of the circle in which the radius is given by the user:"+res);
+
+    }
+
+}
