e344ccd5a63360954c7a3d49363439386853dc93
Nikita Tayade <ntayade9322@gmail.com>
2026-08-24 21:37:56 +0530

    Create Total_surface_area_cuboid.java

---
+++ Total_surface_area_cuboid.java
@@ -0,0 +1,10 @@
+public class Total_surface_area_cuboid {
+    public static void main(String[] args) {
+        int l=9;
+        int b=8;
+        int h=5;
+        int res=l*b*h;
+        System.out.println(res);
+    }
+}
+
