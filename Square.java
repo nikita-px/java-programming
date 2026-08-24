e6a0d12362dca9f10f63766306abd102e1d43aa0
Nikita Tayade <ntayade9322@gmail.com>
2026-08-24 22:39:16 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args){
+
+        //taking the input from the user
+
+        Scanner sc= new Scanner(System.in);
+        double r=sc.nextDouble(); // Accept the Double input ...
+
+        double res=r*r;
+        System.out.println("square  is:"+res);
+
+    }
+
+}
