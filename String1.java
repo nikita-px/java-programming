dac203ab66c715ae3d65e36265ac46ca78112584
Nikita Tayade <ntayade9322@gmail.com>
2026-08-26 11:33:04 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,27 @@
+public class String1 {
+    public static void main (String[] args){
+        String s1="Nikita";
+        //ASCII VALUE
+        // a - 97 ...... z=122
+        //A - 66 .... z=90
+        // 0 - 48 ... 9 - 57
+
+
+//IMPLICIT TYPE CASTING ...
+        char ch = 'A';
+        int x = ch;
+        System.out.println(" the ascii value of the small a is :"+x);
+
+        char ch1 = 'a';
+        int r = ch1;
+        System.out.println(" the ascii value of the small a is :"+r);
+
+      // EXPLICITE TYPE CASTING ..
+
+        char nikita ='a';
+        int y = (int) nikita ;
+        System.out.println("the explicit type casting can be happen with the \n help of the above syntax "+y);
+
+
+    }
+}
