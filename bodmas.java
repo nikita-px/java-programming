e36585478c1942bdcb3439e4b8d23365aaba91c9
Nikita Tayade <ntayade9322@gmail.com>
2026-08-26 11:50:26 +0530

    Add BODMAS calculations in bodmas.java
    Implement BODMAS operations in bodmas.java

---
+++ bodmas.java
@@ -0,0 +1,19 @@
+
+
+
+public class bodmas {
+    public static void main(String[] args){
+        //BODMAS
+
+        // ------> / > * > % > > > + > -
+
+        double nikita = 2*9 /87.0 ;
+        ++nikita;
+        System.out.println(nikita);
+
+        nikita=65+76;
+        --nikita;
+        System.out.println(nikita);
+
+    }
+}
