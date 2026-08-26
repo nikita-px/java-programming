a6c0b63daf190d6f5540110e4855047f4d012ac3
Nikita Tayade <ntayade9322@gmail.com>
2026-08-26 10:50:12 +0530

    Create rate.java

---
+++ rate.java
@@ -0,0 +1,13 @@
+public class rate {
+    public static void main(String[] args){
+        int p=13, r=4,t=9;
+        double p1=p*r*t/100; //4.0
+        System.out.println("principal amount of the 1st :"+p1);
+
+        //After the 100.0 
+
+        double p2=p*r*t/100.0; //4.68
+        System.out.println("Principal amount of the 2nd :"+p2);
+
+    }
+}
